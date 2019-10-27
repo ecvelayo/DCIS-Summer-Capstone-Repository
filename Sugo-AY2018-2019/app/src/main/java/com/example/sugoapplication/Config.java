1ca57bc8e305f6f95d2f3de7db7db4a03ccef958
elvcalatrava25 <elvcalatrava25@gmail.com>
2019-10-28 00:47:44 +0800

    update

--- Sugo-AY2018-2019/app/src/main/java/com/example/sugoapplication/Config.java
+++
@@ -1,6 +0,0 @@
-package com.example.sugoapplication;
-
-public class Config {
-
-    public static final String PAYPAL_CLIENT_ID = "AfXAULnTT2aFzGAtzRgw-kEeW7Swe1FK2H5bMV12tGZabgSk89cJ0XxaXbz5rZowBZATmcy6H4iDVCmt";
-}
